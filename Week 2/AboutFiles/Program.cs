c8c7c8b24151b048d8ba5be8966a86440fe9a7b5
nebman01 <nurhan2808@gmail.com>
2019-02-07 00:51:36 +0600

    improved Task1

---
+++ Week 2/AboutFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AboutFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "First line", "Second line", "Third line" };
+            System.IO.File.WriteAllLines(@"C:\KBTU study\2nd semester\PP2\Week 2\AboutFiles\WriteLines2.txt", lines);
+
+            // using (System.IO.StreamWriter file =
+            //     new System.IO.StreamWriter(@"C:\KBTU study\2nd semester\PP2\Week 2\AboutFiles\WriteLines2.txt", true))
+            // //true - оставляет все что есть в файле и дает что то поделать после всех строк
+            // {
+            //     // foreach(string line in lines)
+            //     // {
+            //     //      if(line.Contains("e"))
+            //     //      {
+            //                 file.WriteLine("Fourth line");
+            //     //      }
+            //     // }
+            // }
+
+            //приравнять файл в новому массиву типа string и вывести на терминал
+            string[] lines2 = System.IO.File.ReadAllLines(@"C:\KBTU study\2nd semester\PP2\Week 2\AboutFiles\WriteLines2.txt");
+            // foreach(string line2 in lines2)
+            //     Console.WriteLine(line2);
+
+            //вывод всех строк в тексте
+            // foreach(string line3 in lines)
+            //     System.Console.WriteLine("Contents of WriteLines2.txt = {0}", line3);
+
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines2)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+        }
+    }
+}
---
+++ Week 2/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some string, and I'll check if it's polindrome:\n");
+
+            string inputstr = Console.ReadLine();
+
+            System.IO.File.WriteAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task1\input.txt", inputstr);
+
+            string ifpol = System.IO.File.ReadAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task1\input.txt");
+
+            int n = ifpol.Length;
+            int a = 0;
+            for(int i = 0; i < n/2; i++){
+                if(ifpol[i] == ifpol[n-i-1]){
+                    a++;
+                }
+            }
+            if(a == n/2)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}

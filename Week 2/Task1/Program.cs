09d515d52c47ad102f00f6cb204f09fbfbf99a82
nebman01 <nurhan2808@gmail.com>
2019-02-16 06:16:35 +0600

    Task2

--- Week 2/Task1/Program.cs
+++ Week 2/Task1/Program.cs
@@ -6,14 +6,14 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter some string, and I'll check if it's polindrome:\n");
+            Console.WriteLine("Enter some string, and I'll check if it's polindrome:\n"); 
 
-            string inputstr = Console.ReadLine();
-
+            string inputstr = Console.ReadLine(); //создаю стринг и ввожу строку через консоль
+            //создаю файл и вывожу в нем строку inputstr:
             System.IO.File.WriteAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task1\input.txt", inputstr);
-
+            //через функцию readAllText приравниваю строку в файле в другую строку:
             string ifpol = System.IO.File.ReadAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task1\input.txt");
-
+            //проверяю на полиндромность(?):
             int n = ifpol.Length;
             int a = 0;
             for(int i = 0; i < n/2; i++){

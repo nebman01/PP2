34102feb3d130f0a9a36c20afbca33d824c91d1e
nebman01 <nurhan2808@gmail.com>
2019-02-16 08:06:39 +0600

    W2T3

--- Week 2/Task2/Program.cs
+++ Week 2/Task2/Program.cs
@@ -11,18 +11,18 @@ namespace Task2
             string inputstr = Console.ReadLine();
 
             System.IO.File.WriteAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task2\input.txt", inputstr);
-
+            //приравниваю строки в файле в новую строку str через функцию ReadAllText
             string str = System.IO.File.ReadAllText(@"C:\KBTU study\2nd semester\PP2\Week 2\Task2\input.txt");
-
+            //создаю массив строк и делю их по умолчанию на пробелы через функцию split:
             string[] sArr = str.Split();
-            int[] intArr = new int[sArr.Length];
-            for (int i = 0; i < sArr.Length; i++)
+            int[] intArr = new int[sArr.Length]; //создаю массив чисел
+            for (int i = 0; i < sArr.Length; i++) 
             {
-                intArr[i] = int.Parse(sArr[i]);
+                intArr[i] = int.Parse(sArr[i]); //через цикл передаю массив строк в массив чисел конвертируя их
             }
 
             int numPr = 0;
-
+            //считаю prime numbers
             for (int i = 0; i < intArr.Length; i++)
             {
                 int x = 0;
@@ -32,14 +32,14 @@ namespace Task2
                     {
                         if (intArr[i] % j == 0)
                         {
-                            x = 1;
-                            break;
+                            x = 1; //делю число на все числа до самого данного числа
+                            break; //если делится без остатка то это не прайм , приравниваем х к 0 и выходим с цикла
                         }
                     }
                     if (x == 0)
                     {
-                        intArr[numPr] = intArr[i];
-                        numPr++;
+                        intArr[numPr] = intArr[i];  //если в цикле все таки до самого числа не поделилось ни на что без остатка
+                        numPr++;                    //приравниваю все прайм числа к началу массива и считаю их количество
                     }
                 }
             }
@@ -49,13 +49,14 @@ namespace Task2
             for (int i = 0; i < numPr; i++)
             {
                 int b = intArr[i];
-                strOutput[i] = Convert.ToString(b);
+                strOutput[i] = Convert.ToString(b); //передаю прайм числа которые находились в начале массива в массив строк
             }
 
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\KBTU study\2nd semester\PP2\Week 2\Task2\output.txt"))
             {
+                // создаю новый файл и передаю туда весь массив строк из прайм чисел через цикл
 				foreach(string line in strOutput)
-					file.Write(line + " ");
+					file.Write(line + " ");  
             }
         }
     }
--- Week 2/Task3/Program.cs
+++ Week 2/Task3/Program.cs
@@ -9,27 +9,28 @@ namespace Task3
         {
             for (int i = 0; i < level; i++)
             {
-                Console.Write("     ");
+                Console.Write("     "); //чтобы было читабельно делаю табуляцию
             }
         }
 
         public static void func(DirectoryInfo directory, int level)
         {
-            foreach (FileInfo f in directory.GetFiles())
+            foreach (FileInfo f in directory.GetFiles())  //работаю с файлами в массиве directory
             {
-                PrintSpaces(level);
-                Console.WriteLine(f.Name);
+                PrintSpaces(level); //делаю табуляцию 
+                Console.WriteLine(f.Name); //вывожу имя файлов
             }
-            foreach(DirectoryInfo d in directory.GetDirectories()){
+            foreach(DirectoryInfo d in directory.GetDirectories()){ //работаю с папками в массиве directory
                 PrintSpaces(level);
-                Console.WriteLine(d.Name);
-                func(d, level+1);
+                Console.WriteLine(d.Name); //вывожу имя папки в directory
+                func(d, level+1); //открываю папку через рекурсию и вывожу все что в этой папке и так пока не закончатся все папки и файлы в папке
             }
         }
 
         static void Main(string[] args)
         {
-            func(new DirectoryInfo("C:/KBTU study/2nd semester/PP2/Week 2/Task3"), 0);
+            //передаю в функцию дайректори и число для табуляций
+            func(new DirectoryInfo("C:/KBTU study/2nd semester/PP2/Week 2/Task3"), 0); 
         }
     }
 }
